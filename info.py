d483936470a836f09f61a37b346f0a82ae312c7c
ru4903893 <154866396+ru4903893@users.noreply.github.com>
2024-12-20 11:11:42 +0400

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
-PICS = (environ.get('PICS', 'https://envs.sh/8dC.jpg')).split()
+PICS = (environ.get('PICS', 'https://envs.sh/7e8.jpg')).split()
 
 
 # Admins & Users
